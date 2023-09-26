7f6c73021618cf2d7a929d27fefea4076e26c2be
mexzzy <samuelmeshach055@gmail.com>
2023-09-26 14:39:07 +0100

    final changes

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,21 +1,66 @@
 import { Slide, Zoom, Bounce } from "react-awesome-reveal";
 import "../CSS page/Contact.css";
+import emailjs from "emailjs-com";
+import { useState } from "react";
 
 const Form = () => {
+
+  const [showSuccess, setShowSuccess] = useState(false);
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_EMAILJS_SERVICE_ID,
+        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+        e.target,
+        process.env.REACT_APP_EMAILJS_USER_ID
+      )
+      .then(
+        (result) => {
+          console.log('Email sent successfully:', result.text);
+          setShowSuccess(true);
+          setTimeout(() => {
+            setShowSuccess(false);
+          }, 7000);
+        },
+        (error) => {
+          console.error("Email could not be sent:", error.text);
+        }
+      );
+  }
   return (
     <>
-      <form>
+      <form onSubmit={sendEmail}>
+      {showSuccess && (
+        <div className="success-message">
+          Email message sent successfully!
+        </div>
+      )}
         <Slide triggerOnce>
           <div className="container">
             <span> full name</span>
-            <input id="name" type="text" placeholder="-" required />
+            <input
+              id="name"
+              name="user_name"
+              type="text"
+              placeholder="-"
+              required
+            />
           </div>
         </Slide>
         <Slide triggerOnce direction="right">
           <div className="formFlex">
             <div className="container">
               <span> email</span>
-              <input id="email" type="email" placeholder="-" required />
+              <input
+                id="email"
+                name="user_email"
+                type="email"
+                placeholder="-"
+                required
+              />
             </div>
           </div>
         </Slide>
@@ -35,7 +80,7 @@ const Form = () => {
         </Zoom>
         <Bounce triggerOnce>
           <div className="formBtn">
-            <button>send</button>
+            <button type="submit">send</button>
           </div>
         </Bounce>
       </form>
