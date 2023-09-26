2fef98ce343c77bf67cf68960a7c3c46e53f9b39
mexzzy <samuelmeshach055@gmail.com>
2023-09-26 18:05:25 +0100

    final

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,105 +1,106 @@
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
+import { Circles } from "react-loader-spinner";
+// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import { Slide, Zoom, Bounce } from "react-awesome-reveal";
 import "../CSS page/Contact.css";
-import emailjs from "emailjs-com";
-import { useState } from "react";
 
 const Form = () => {
   const [showSuccess, setShowSuccess] = useState(false);
   const [showNotSuccess, setShowNotSuccess] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  function sendEmail(e) {
+  const sendEmail = async (e) => {
     e.preventDefault();
     setLoading(true);
 
-    emailjs
-      .sendForm(
+    try {
+      const result = await emailjs.sendForm(
         process.env.REACT_APP_EMAILJS_SERVICE_ID,
         process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
         e.target,
         process.env.REACT_APP_EMAILJS_USER_ID
-      )
-      .then(
-        (result) => {
-          console.log("Email sent successfully:", result.text);
-          setShowSuccess(true);
-          setTimeout(() => {
-            setShowSuccess(false);
-          }, 7000);
-        },
-        (error) => {
-          console.error("Email could not be sent:", error.text);
-          setShowNotSuccess(true);
-          setTimeout(() => {
-            setShowNotSuccess(false);
-          }, 7000);
-        }
-      )
-      .finally(() => {
-        setLoading(false);
-      });
-  }
+      );
+      console.log("Email sent successfully:", result.text);
+      setShowSuccess(true);
+      setTimeout(() => {
+        setShowSuccess(false);
+      }, 7000);
+    } catch (error) {
+      console.error("Email could not be sent:", error.text);
+      setShowNotSuccess(true);
+      setTimeout(() => {
+        setShowNotSuccess(false);
+      }, 7000);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
-    <>
-      <form onSubmit={sendEmail}>
-        {showSuccess && (
-          <div className="success-message">
-            Email message sent successfully!
-          </div>
-        )}
-        {showNotSuccess && (
-          <div className="errorMsg">Email message not sent successfully!</div>
-        )}
-        <Slide triggerOnce>
+    <form onSubmit={sendEmail}>
+      {showSuccess && (
+        <div className="success-message">Email message sent successfully!</div>
+      )}
+      {showNotSuccess && (
+        <div className="errorMsg">Email message sending failed!</div>
+      )}
+      <Slide triggerOnce>
+        <div className="container">
+          <span>Full Name</span>
+          <input
+            id="name"
+            name="user_name"
+            type="text"
+            placeholder="-"
+            required
+          />
+        </div>
+      </Slide>
+      <Slide triggerOnce direction="right">
+        <div className="formFlex">
           <div className="container">
-            <span> full name</span>
+            <span>Email</span>
             <input
-              id="name"
-              name="user_name"
-              type="text"
+              id="email"
+              name="user_email"
+              type="email"
               placeholder="-"
               required
             />
           </div>
-        </Slide>
-        <Slide triggerOnce direction="right">
-          <div className="formFlex">
-            <div className="container">
-              <span> email</span>
-              <input
-                id="email"
-                name="user_email"
-                type="email"
-                placeholder="-"
-                required
-              />
-            </div>
-          </div>
-        </Slide>
-        <br />
-
-        <Zoom triggerOnce>
-          <div className="container">
-            <span>message</span>
-            <textarea
-              required
-              name="message"
-              id="message"
-              cols="30"
-              rows="10"
-            ></textarea>
-          </div>
-        </Zoom>
-        <Bounce triggerOnce>
-          <div className="formBtn">
-            <button type="submit" disabled={loading}>
-              {loading ? "Sending..." : "Send"}
-            </button>
-          </div>
-        </Bounce>
-      </form>
-    </>
+        </div>
+      </Slide>
+      <br />
+      <Zoom triggerOnce>
+        <div className="container">
+          <span>Message</span>
+          <textarea required name="message" id="message" cols="30" rows="10" />
+        </div>
+      </Zoom>
+      <Bounce triggerOnce>
+        <div className="formBtn">
+          <button type="submit" disabled={loading}>
+            {loading ? (
+              <div className="btnFlex"> 
+                <span>Sending... </span>
+                <Circles
+                  height="20"
+                  width="20"
+                  color="#fff"
+                  ariaLabel="circles-loading"
+                  wrapperStyle={{}}
+                  wrapperClass=""
+                  visible={true}
+                />
+              </div>
+            ) : (
+              "Send"
+            )}
+          </button>
+        </div>
+      </Bounce>
+    </form>
   );
 };
 
